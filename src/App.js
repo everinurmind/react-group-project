085e04ef29f7c5130f13ca34f0d2cf721505c3d2
everinurmind <nurbol.sultanov@gmail.com>
2023-04-17 15:31:25 -0600

    Add NavLink

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-     
+      <Navigation />
+      <Routes>
+        <Route path="/Rockets" element={<Rockets />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../logo/planet.png';
+import '../styles/Navigation.css';
+
+const Navigation = () => (
+  <header>
+    <img alt="logo" src={logo} />
+    <h1>Space Traveller&apos;s Hub</h1>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/Rockets">Rockets</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Missions">Missions</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Profile">My Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Navigation;
