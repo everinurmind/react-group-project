2ddec90b920fd19688a285cef52b6980a6c2f9a8
everinurmind <nurbol.sultanov@gmail.com>
2023-04-18 14:19:41 -0600

    Fixed Conflicting Files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import { rocketsReducer } from './redux/rocketsSlice';
+import { rocketsReducer } from './rocketsSlice';
 
 const rootReducer = combineReducers({
   rockets: rocketsReducer,
