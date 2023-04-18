5af5707d6d91f8a61530451e8d1054bded8af206
everinurmind <nurbol.sultanov@gmail.com>
2023-04-18 12:28:10 -0600

    Wrap app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Profile from './components/Profile';
+import { setRockets } from './redux/rocketsSlice';
+
+const fetchRockets = async (dispatch) => {
+  const response = await fetch('https://api.spacexdata.com/v3/rockets');
+  const data = await response.json();
+  dispatch(setRockets(data));
+};
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchRockets(dispatch);
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Navigation />
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import { rocketsReducer } from './redux/rocketsSlice';
+
+const rootReducer = combineReducers({
+  rockets: rocketsReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
+
+export default store;
