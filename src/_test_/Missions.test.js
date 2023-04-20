9c116eba267bdc2d86f999f1e3dc71e8ab90fd9b
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-04-21 03:12:17 +0500

    Add tests for missions

---
+++ src/_test_/Missions.test.js
@@ -0,0 +1,48 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { getMissions, joinMission, leaveMission } from 'redux/missions/missionsSlice';
+
+const mockStore = configureMockStore([thunk]);
+
+describe('testing missionsSlice', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      missions: {
+        missions: [],
+        isLoading: false,
+        error: '',
+      },
+    });
+  });
+
+  it('should get missions from API', async () => {
+    const expectedActions = [
+      getMissions.pending.type,
+      getMissions.fulfilled.type,
+    ];
+
+    await store.dispatch(getMissions());
+    const actions = store.getActions().map((action) => action.type);
+    expect(actions).toEqual(expectedActions);
+  });
+
+  it('should join a mission', () => {
+    const id = 763;
+    const expectedAction = {
+      type: joinMission.type,
+      payload: id,
+    };
+    expect(joinMission(id)).toEqual(expectedAction);
+  });
+
+  it('should leave a mission', () => {
+    const id = 763;
+    const expectedAction = {
+      type: leaveMission.type,
+      payload: id,
+    };
+    expect(leaveMission(id)).toEqual(expectedAction);
+  });
+});
